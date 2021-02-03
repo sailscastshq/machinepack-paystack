49b958c5b906b2c883a744da7cb3cb5fa4e4fae0
Kelvin Omereshone <kelvinomereshone@gmail.com>
2021-02-03 10:36:59 +0100

    feat: :sparkles: remove useless try/catch block

--- helpers/make-request.js
+++ helpers/make-request.js
@@ -1,12 +1,8 @@
 const fetch = require('node-fetch')
-const makeRequest = async  (url, options = {}) => {
-  try {
-    const response = await fetch(`https://api.paystack.co${url}`, options)
-    const json = await response.json()
-    return json
-  } catch (error) {
-    throw error
-  }
+const makeRequest = async (url, options = {}) => {
+  const response = await fetch(`https://api.paystack.co${url}`, options)
+  const json = await response.json()
+  return json
 }
 
 exports.makeRequest = makeRequest
