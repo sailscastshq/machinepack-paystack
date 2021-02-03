9169f5bd9d2f189f16a452585de3889907c58083
Kelvin Omereshone <kelvinomereshone@gmail.com>
2021-02-03 10:38:26 +0100

    feat: :sparkles: remove preparation code for test into Mocha hooks

--- test/machines/create-plan.test.js
+++ test/machines/create-plan.test.js
@@ -1,11 +1,6 @@
-const Paystack = require('../../')
-if (!process.env.PAYSTACK_API_KEY_FOR_TESTS) {
-  throw new Error('In order to run tests, the `PAYSTACK_API_KEY_FOR_TESTS` environment variable must be set!')
-}
-
 describe('Paystack.createPlan()', () => {
   it('Successfully create a plan', (done) => {
-    Paystack.createPlan({
+    global.Paystack.createPlan({
       apiKey: process.env.PAYSTACK_API_KEY_FOR_TESTS,
       name: 'Test Plan',
       amount: 10000,
