0f14278ae80901ae02144c9915a1da6848f707bc
Kelvin Omereshone <kelvinomereshone@gmail.com>
2021-02-03 10:39:34 +0100

    test: :white_check_mark: add check for apiToken and set global.Paystack in beforeAll hook

--- test/hooks.js
+++ test/hooks.js
@@ -1,3 +1,10 @@
 require('dotenv').config()
 exports.mochaHooks = {
+  beforeAll (done) {
+    if (!process.env.PAYSTACK_API_KEY_FOR_TESTS) {
+      throw new Error('In order to run tests, the `PAYSTACK_API_KEY_FOR_TESTS` environment variable must be set!')
+    }
+    global.Paystack = require('../')
+    done()
+  }
 }
