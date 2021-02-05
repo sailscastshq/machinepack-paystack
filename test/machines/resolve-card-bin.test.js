d4bf93ce0a06645d9944439b08dac83e9713cc77
Kelvin Omereshone <kelvinomereshone@gmail.com>
2021-02-05 20:10:19 +0100

    test: :white_check_mark: add test for resolve-card-bin machine

---
+++ test/machines/resolve-card-bin.test.js
@@ -0,0 +1,22 @@
+describe('Paystack.resolveCardBin()', () => {
+  it('Successfully resolved card bin', (done) => {
+    global.Paystack.resolveCardBin({
+      apiKey: process.env.PAYSTACK_API_KEY_FOR_TESTS,
+      bin: '539983'
+    }).exec(function (error, _) {
+      if (error) return done(error)
+      return done()
+    })
+  })
+
+  it.only('fails to resolve bin with wrong apiKey', (done) => {
+    global.Paystack.listPlans({
+      apiKey: 'absolutely_FAKEY!!',
+      bin: '539983'
+    }).exec(function (error, response) {
+      if (error) return done(error)
+      if (response.message === 'Invalid key') return done()
+      return done(response)
+    })
+  })
+})
