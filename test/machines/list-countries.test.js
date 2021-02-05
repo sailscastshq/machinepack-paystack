e6b2e35469352beea39cc083b80bf3e363b4d833
Kelvin Omereshone <kelvinomereshone@gmail.com>
2021-02-05 19:48:18 +0100

    test: :white_check_mark: remove .only from test case

--- test/machines/list-countries.test.js
+++ test/machines/list-countries.test.js
@@ -1,5 +1,5 @@
 describe('Paystack.listCountries()', () => {
-  it.only('Successfully retrieved countries', (done) => {
+  it('Successfully retrieved countries', (done) => {
     global.Paystack.listCountries({
     }).exec(function (error, _) {
       if (error) return done(error)
