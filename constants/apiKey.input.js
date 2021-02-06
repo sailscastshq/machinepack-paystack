17a5dd6d8570e28a83d740aa56e4aafbc73777c7
Kelvin Omereshone <kelvinomereshone@gmail.com>
2021-02-06 11:35:53 +0100

    fix: :memo: correct input example

--- constants/apiKey.input.js
+++ constants/apiKey.input.js
@@ -6,6 +6,6 @@ module.exports = {
     description: 'Copy either "Test Secret Key" or "Live Secret Key" from your Paystack dashboard.',
     extendedDescription: 'Make sure you are logged in to your Paystack account, or create an account if you have not already done so.'
   },
-  example: 'pk_test_f01df68ffa23d136959f2c532c583f7be2763932',
+  example: 'sk_test_f01df68ffa23d136959f2c532c583f7be2763932',
   protect: true
 }
