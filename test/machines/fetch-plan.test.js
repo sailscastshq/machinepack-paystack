641878f3f7d80ebe0767da9e7ce32ca072965950
Kelvin Omereshone <kelvinomereshone@gmail.com>
2021-02-06 17:22:04 +0100

    test: :white_check_mark: add test for fetch-plan

---
+++ test/machines/fetch-plan.test.js
@@ -0,0 +1,11 @@
+describe('Paystack.fetchPlan()', () => {
+  it('Successfully retrieved plan', (done) => {
+    global.Paystack.fetchPlan({
+      apiKey: process.env.PAYSTACK_API_KEY_FOR_TESTS,
+      id_or_code: 'PLN_gx2wn530m0i3w3m'
+    }).exec(function (error, _) {
+      if (error) return done(error)
+      return done()
+    })
+  })
+})
