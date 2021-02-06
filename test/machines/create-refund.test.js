ca0bb88ec45842fa17ae2102c23d4f1329d3044b
Kelvin Omereshone <kelvinomereshone@gmail.com>
2021-02-06 16:14:56 +0100

    test: :white_check_mark: add test for create-refund machine

---
+++ test/machines/create-refund.test.js
@@ -0,0 +1,11 @@
+describe('Paystack.createRefund()', () => {
+  it.only('Successfully create a refund', (done) => {
+    global.Paystack.createRefund({
+      apiKey: process.env.PAYSTACK_API_KEY_FOR_TESTS,
+      transaction: 'x7g15k5iye'
+    }).exec(function (error, _) {
+      if (error) return done(error)
+      return done()
+    })
+  })
+})
