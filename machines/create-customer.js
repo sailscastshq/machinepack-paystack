a2435d1a52a30acbd115d7631f536ccbfe4590bb
Haastrup ELijah <haastrupea@gmail.com>
2021-02-12 15:16:01 +0100

    Feat: add Machine to create customer

---
+++ machines/create-customer.js
@@ -0,0 +1,59 @@
+const { getHeaders } = require('../helpers/get-headers')
+const { makeRequest } = require('../helpers/make-request')
+
+module.exports = {
+
+  friendlyName: 'Create Customer',
+
+  description: 'Create a customer on your integration',
+
+  cacheable: false,
+
+  sync: false,
+
+  inputs: {
+    email: {
+      example: 'customer@email.com',
+      description: 'Customer\'s email address'
+    },
+    first_name: {
+      example: 'john',
+      description: 'Customer\'s first name'
+    },
+    last_name: {
+      example: 'doe',
+      description: 'Customer\'s last name'
+    },
+    phone: {
+      example: '23481363647484',
+      description: 'Customer\'s phone number'
+    },
+    metadata: {
+      type: 'ref',
+      example: {},
+      description: 'A set of key/value pairs that you can attach to the customer. It can be used to store additional information in a structured format.'
+    }
+  },
+
+  exits: {
+
+    success: {
+      description: 'Customer Created successfully.'
+    }
+
+  },
+
+  fn: function ({ apiKey, ...bodyParams }, exits) {
+    makeRequest('/customer',
+      {
+        method: 'POST',
+        headers: getHeaders(apiKey || process.env.PAYSTACK_API_KEY),
+        body: JSON.stringify(bodyParams)
+      }).then((response) => {
+      return exits.success(response)
+    }).catch(error => {
+      return exits.error(error)
+    })
+  }
+
+}
